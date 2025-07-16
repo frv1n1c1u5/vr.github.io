a65951d226cbe09841261c916f4dd6e1630336f5
frv1n1c1u5 <vinicius.farias.rocha@gmail.com>
2025-07-16 16:15:20 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,8 @@
 // Arquivo: js/supabaseClient.js
 // VERSÃO FINAL COM O PAR DE URL E CHAVE 100% CORRETO
 
-const SUPABASE_URL = 'https://jnpgajezpvozelaieb.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpucGdvamplenB2b3plbGFpZWIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTcyMTE1OTY5MywiZXhwIjoyMDM2NzM1NjkzfQ.B53v0N8g1rQGZcZ1eX3r9y8v7o5Y5e5i7I4t8l2k4p4';
+const SUPABASE_URL = 'https://nvtfpqtfhsvbezcbxgzt.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52dGZwcXRmaHN2YmV6Y2J4Z3p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI2OTI4NjcsImV4cCI6MjA2ODI2ODg2N30.1AIeEPsOUktmfqQVbmb5N_sYGy5Tfj6PNdHSSJhXOeE';
 
 // Acessamos a função global 'supabase.createClient' e atribuímos
 // o cliente a uma nova variável com nome diferente ('sbClient') para evitar conflito.
