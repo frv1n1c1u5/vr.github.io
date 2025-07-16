b4690fb39cd9684cd148519f7aa4390f43e529d5
frv1n1c1u5 <vinicius.farias.rocha@gmail.com>
2025-07-16 15:30:41 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 // Arquivo: js/supabaseClient.js
-// VERSÃO FINAL CORRIGIDA
+// VERSÃO FINAL E CORRIGIDA
 
 const SUPABASE_URL = 'https://rgchiodtvblkcjclrchli.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnY2hpb2R0dmJsa2NjaXJjaGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0OTY5MDMsImV4cCI6MjAzNzA3MjkwM30.L8vjS2yYcQ8aN-9C5DkC-q0g_O1fQ9gW1vD5E6Z2b1Y';
